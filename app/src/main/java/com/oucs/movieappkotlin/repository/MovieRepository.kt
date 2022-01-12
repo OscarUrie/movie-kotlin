d083adffd3a829fc67c5895105103db64014f9dc
OscarUrie <oscar_c_s@hotmail.com>
2022-01-11 23:07:16 -0600

    Retrofit con corrutinas

--- app/src/main/java/com/oucs/movieappkotlin/repository/MovieRepository.kt
+++ app/src/main/java/com/oucs/movieappkotlin/repository/MovieRepository.kt
@@ -1,33 +1,21 @@
 package com.oucs.movieappkotlin.repository
 
-import android.widget.Toast
-import com.oucs.movieappkotlin.common.MovieApp
+import android.util.Log
 import com.oucs.movieappkotlin.retrofit.MovieNetWork
 import com.oucs.movieappkotlin.retrofit.TheMovieDBService
-import com.oucs.movieappkotlin.retrofit.models.Movie
-import com.oucs.movieappkotlin.retrofit.models.MoviesResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieRepository {
     private val theMovieDBService:TheMovieDBService = MovieNetWork.getTheMovieDBService()
-    fun getPopularMovies(callback:(List<Movie>?) -> Unit)  {
-        val response = theMovieDBService.getPopularMovies()
-        response.enqueue(object : Callback<MoviesResponse> {
-            override fun onResponse(call: Call<MoviesResponse>, response: Response<MoviesResponse>) {
-                if (response.isSuccessful){
-                    callback(response.body()?.results)
-                }
-                else{
-                    Toast.makeText(MovieApp.instance,"Algo salio mal",Toast.LENGTH_SHORT).show()
-                    callback(null)
-                }
-            }
-            override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                Toast.makeText(MovieApp.instance,"Error de conexion",Toast.LENGTH_SHORT).show()
-                callback(null)
-            }
-        })
+    suspend fun getPopularMovies() = withContext(Dispatchers.IO){
+        try {
+            val response = theMovieDBService.getPopularMovies()
+            return@withContext response.body()?.results
+        }
+        catch (e:Exception){
+            Log.e("Retrofit","${e.printStackTrace()}")
+            return@withContext null
+        }
     }
 }
--- app/src/main/java/com/oucs/movieappkotlin/ui/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/oucs/movieappkotlin/ui/viewmodel/MovieViewModel.kt
@@ -1,9 +1,11 @@
 package com.oucs.movieappkotlin.ui.viewmodel
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.oucs.movieappkotlin.common.MovieApp
 import com.oucs.movieappkotlin.repository.MovieRepository
 import com.oucs.movieappkotlin.retrofit.models.Movie
 import kotlinx.coroutines.launch
@@ -15,10 +17,12 @@ class MovieViewModel:ViewModel() {
     @SuppressLint("NullSafeMutableLiveData")
     fun loadPopularMovies(){
         viewModelScope.launch {
-            movieRepository.getPopularMovies{ popular ->
-                if (!popular.isNullOrEmpty()){
-                    popularMovies.value = popular
-                }
+            val popular = movieRepository.getPopularMovies()
+            if (!popular.isNullOrEmpty()){
+                popularMovies.value = popular
+            }
+            else{
+                Toast.makeText(MovieApp.instance,"Algo salio mal", Toast.LENGTH_SHORT).show()
             }
         }
     }
